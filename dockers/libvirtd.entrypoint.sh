698e46c8b126c0ddad6a0d9a3dd38e79e62f41af
Chung Chih, Hung <lyanchih@gmail.com>
2017-05-02 15:41:57 +0800

    virtlogd will still running after libvirt container destroy
    Kill the virtlogd process by its pid file

--- dockers/libvirtd.entrypoint.sh
+++ dockers/libvirtd.entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# NOTE(lyanchih) virtlogd will probablly running after container destroy
+if [ -f /var/run/virtlogd.pid ]; then
+    kill $(cat /var/run/virtlogd.pid) || true
+fi
+
 # NOTE(lyanchih) libvirt version after 1.16 mandatory virtlog daemon
 virtlogd -d
 
