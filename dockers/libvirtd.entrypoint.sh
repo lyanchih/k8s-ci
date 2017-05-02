548a6e7301e87f7843d6136f6305d57e4f78f8ec
Chung Chih, Hung <lyanchih@gmail.com>
2017-05-02 18:22:02 +0800

    Revert commit 698e46c8b126c0ddad6a0d9a3dd38e79e62f41af

--- dockers/libvirtd.entrypoint.sh
+++ dockers/libvirtd.entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-set -e
-
-# NOTE(lyanchih) virtlogd will probablly running after container destroy
-if [ -f /var/run/virtlogd.pid ]; then
-    kill $(cat /var/run/virtlogd.pid) || true
-fi
-
 # NOTE(lyanchih) libvirt version after 1.16 mandatory virtlog daemon
 virtlogd -d
 
